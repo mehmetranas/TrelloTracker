a849400e5408303dce0f3cbd61d9883120da9b99
mehmetranas <mhmtranas@gamil.com>
2017-11-14 14:10:01 +0300

    Added all service and fixed

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-service();
+var key ="eab85d16a611da0505d6feea7184e16c",
+    token = "2cd190dcfd2850a178cf48e52afe4ea49d3102bd2d05b3c389359d9dee485cc1",
+    boardId = "59fa17caf755d8746621c77b";
+
+$(document).ready(function () {
+
+    listService(boardId,function (lists) {
+        cardService(lists);
+    });
+    });
+
+
+
+
--- service.js
+++ service.js
@@ -1,68 +1,71 @@
-var service = function () {
-    var key ="eab85d16a611da0505d6feea7184e16c",
-        token = "2cd190dcfd2850a178cf48e52afe4ea49d3102bd2d05b3c389359d9dee485cc1",
-        boardId = "59fa17caf755d8746621c77b";
-    var url = "https://api.trello.com/1/boards/59fa17caf755d8746621c77b/lists/?limit=2&fields=name&members=true&member_fields=fullName&key=" + key + "&token="+ token;
-
-
 var fillTemplate = function (data) {
+    data.forEach(function (list) {
+        var value = 0;
+        list.cards.forEach(function (card) {
+            value += parseInt(card.value);
+        });
+        list.total = value;
+        if(value >= parseInt(list.categoryLimit)*(0.8) && value < parseInt(list.categoryLimit)){
+            list.condition = "danger";
+        }else if (value >= parseInt(list.categoryLimit)){
+            list.condition = "very-danger";
+        }
+    });
+
     var content = $("#template").html();
     var result = _.template(content)({data:data});
     $(".list").html(result);
 };
 
-var func = function (lists) {
-    cards(lists);
-  };
-
+var listService = function (id,callback) {
+    var url = "https://api.trello.com/1/boards/" + id + "/lists/?limit=2&fields=name&members=true&member_fields=fullName&key=" + key + "&token="+ token;
     $.ajax({
         method:"GET",
         url:url,
-        success:func,
+        success:callback,
         error: function () {
             console.log("Error");
         }
     });
+};
 
-    var cards = function (lists,callback) {
-        var allLists = [];
-        var cardService = function (listParam) {
-            $.ajax({
-                method:"GET",
-                url:"https://api.trello.com/1/lists/" + listParam.id +"/cards/?fields=name&members=true&member_fields=fullName&key=" + key + "&token="+ token,
-                success:  function (result) {
-                    var listDetail = function (listName,cards) {
-                        this.listName = listName;
-                        this.cards = cards;
-                    };
+var cardService = function (lists) {
+    var newLists = [];
+    var ajaxRemainder = lists.length;
 
-                    var cardDetail = [];
-                    result.forEach(function (card, p2, p3) {
-                            var name = card.name.split(" ")[0];
-                            var value = card.name.split(" ")[1];
-                            cardDetail.push({name:name,value:value});
-                    });
-                        var listResult = new listDetail(listParam.name,cardDetail);
-                        allLists.push(listResult);
-                },
-                error: function () {
-                    console.log("Error");
-                }
-            })
+    lists.forEach(function (list) {
+        var cardsLocal = {
+            category: list.name.split("Max:")[0],
+            categoryLimit: list.name.split("Max:")[1],
+            total:0,
+            cards: [],
+            condition:"",
         };
+        var url = "https://api.trello.com/1/lists/" + list.id +"/cards/?fields=name&members=true&member_fields=fullName&key=" + key + "&token="+ token;
+        $.ajax({
+            method:"GET",
+            url:url,
+            success:function (cards) {
+                for(var j = 0; j<cards.length; j++) {
+                    var card = cards[j];
 
-        lists.forEach(function (lst) {
-           cardService(lst);
-        });
-
-        setTimeout(function () {
-            fillTemplate(allLists);
-            console.log(allLists)
-        },5000)
-
-
-    }
-
+                    var cardPush = {
+                        title: card.name.split(" ")[0],
+                        value: card.name.split(" ")[1]
+                    };
+                    cardsLocal.cards.push(cardPush);
+                };
+                newLists.push(cardsLocal)
+                ajaxRemainder--;
+                if(ajaxRemainder == 0) {
+                    fillTemplate(newLists)
+                }
+            },
+            error: function () {
+                console.log("Error");
+            }
+        })
+    }) ;
 };
 
 
