274acc1e1f289a16e729c564f3ef1a4ca110149c
mehmetranas <mhmtranas@gmail.com>
2019-10-08 22:41:57 +0300

    update api and fix

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,7 @@ var key ="eab85d16a611da0505d6feea7184e16c",
     username,
     boardName;
 
-$.fn.extend({n
+$.fn.extend({
     animateCss: function (animationName, callback) {
         var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
         this.addClass('animated ' + animationName).one(animationEnd, function() {
